8e2813385df0c564751cec9e1018bc6f32c81ff9
Drazen08 <drazen08@163.com>
2018-11-04 22:46:23 +0800

    c2

---
+++ src/main/java/com/study/demo/c2/Solution.java
@@ -0,0 +1,42 @@
+package com.study.demo.c2;
+
+/**
+ * Created by S on 2018/11/4.
+ */
+public class Solution {
+    /**
+     * You are given two non-empty linked lists representing two non-negative integers.
+     * The digits are stored in reverse order and each of their nodes contain a single digit.
+     * Add the two numbers and return it as a linked list.
+     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     */
+
+    /**
+     * 给定两个非空链表，表示两个非负整数。这些数字以相反的顺序存储，
+     * 它们的每个节点都包含一个数字。
+     * 添加这两个数字并以链表的形式返回。
+     * 您可以假设这两个数字不包含任何前导零，除了数字0本身。
+     * <p>
+     * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * Output: 7 -> 0 -> 8
+     */
+
+    /**
+     * 解决方案
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int x) {
+            this.val = x;
+        }
+    }
+}
