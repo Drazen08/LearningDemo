e852a9d6bb0281259068fc8ad3661e007cff8f4e
3524606374@qq.com <3524606374@qq.com>
2019-01-19 16:30:49 +0800

    luee

--- src/main/java/com/study/demo/c3/Solution.java
+++ src/main/java/com/study/demo/c3/Solution.java
@@ -1,6 +1,11 @@
 package com.study.demo.c3;
 
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by S on 2018/11/7.
  */
@@ -31,28 +36,20 @@ class Solution {
      * @return
      */
     public int lengthOfLongestSubstring(String s) {
-        int result = 0;
-        int index = 0;
+        Set<Character> current = new HashSet<Character>();
+        int max = 0;
         for (int i = 0; i < s.length(); i++) {
-            String c = s.substring(i, i + 1);
-            String r = s.substring(index, i + 1);
-            System.out.println(c);
-            System.out.println(r);
-            System.out.println(r.lastIndexOf(c));
-            if (r.indexOf(c) != i) {
-                System.out.println("hi");
-                index = i;
-                result = 0;
-            } else {
-                result++;
+            if (i + 1 == s.length()) {
+                break;
             }
+
         }
-        return result;
+        return max;
     }
 
 
     public static void main(String[] args) {
-        String param = "pwwkew";
+        String param = "pwwlew";
         Solution s = new Solution();
         int r = s.lengthOfLongestSubstring(param);
         System.out.println(r);
