78cbcf11dfd11bf20c37d518ab3c0932edd98a96
Drazen08 <drazen08@163.com>
2019-11-15 15:31:44 +0800

    1

--- src/main/java/com/study/leetcode/c5/Solution.java
+++ src/main/java/com/study/leetcode/c5/Solution.java
@@ -5,20 +5,46 @@ package com.study.leetcode.c5;
  * @author: sunjx
  * @create: 2019/11/12
  */
-public class Solution {
+class Solution {
     /**
      * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
-     *
+     * <p>
      * 示例 1：
-     *
+     * <p>
      * 输入: "babad"
      * 输出: "bab"
      * 注意: "aba" 也是一个有效答案。
      * 示例 2：
-     *
+     * <p>
      * 输入: "cbbd"
      * 输出: "bb"
-     *
-     *
      */
+
+    /**
+     * 摊牌了，这个题我看都看不懂
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
 }
