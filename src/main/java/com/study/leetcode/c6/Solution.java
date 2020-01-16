17b357ef6f33b7806dfbe22a6ea0537a6997f1b8
Drazen08 <drazen08@163.com>
2020-01-16 14:44:25 +0800

    -1

--- src/main/java/com/study/leetcode/c6/Solution.java
+++ src/main/java/com/study/leetcode/c6/Solution.java
@@ -1,5 +1,8 @@
 package com.study.leetcode.c6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @version V1.0
  * @author: sunjx
@@ -46,9 +49,9 @@ public class Solution {
         boolean f = true;
         StringBuffer result = new StringBuffer();
         for (int i = 1; i <= (x + y); i++) {
-            if(f){
+            if (f) {
 
-            }else{
+            } else {
 
             }
         }
@@ -56,4 +59,40 @@ public class Solution {
         return null;
     }
 
+    /**
+     * 按行排序
+     */
+    public String convert1(String s, int numRows) {
+        if (numRows == 1) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        // 确定行数，占好格子
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+
+
+    public String convert2(String s,int numRows){
+
+
+        return null;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(1 / 3);
+    }
 }
---
+++ src/main/java/com/study/thinking/Initlise.java
@@ -0,0 +1,52 @@
+package com.study.thinking;
+
+/**
+ * 内部类
+ *
+ * @version V1.0
+ * @author: sunjx
+ * @create: 2019/12/17
+ */
+public class Initlise {
+    abstract class Contents {
+        abstract public int value();
+    }
+
+    interface Destination {
+        String readLabel();
+    }
+
+    static class Parcel3 {
+        private class PContents extends Contents {
+            private int i = 11;
+
+            @Override
+            public int value() {
+                return i;
+            }
+        }
+
+        protected class PDestination implements Destination {
+            private String label;
+
+            private PDestination(String whereTo) {
+                label = whereTo;
+            }
+
+            @Override
+            public String readLabel() {
+                return label;
+            }
+        }
+
+        public Destination dest(String s) {
+            return new PDestination(s);
+        }
+
+        public Contents cont() {
+            return new PContents();
+        }
+    }
+
+
+}
